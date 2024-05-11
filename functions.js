b0fb5c22b11d9d4841ab082d50acf88dad960e0d
Harshit Rai <workwithharshitrai@gmail.com>
2024-05-11 23:46:30 +0530

    Scope in Javascript

--- functions.js
+++ functions.js
@@ -23,4 +23,66 @@ function loginUserMessage(username){
     return `${username} just logged in`;
 }
 console.log(loginUserMessage("Harshit")); // Output  Harshit just logged in
-console.log(loginUserMessage());
+console.log(loginUserMessage()); //Output Please enter user name
+
+
+///Scopes in javascript
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Harshit"
+
+    function two(){
+        const website = "google"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "Harshit"
+    if (username === "Harshit") {
+        const website = " google"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
