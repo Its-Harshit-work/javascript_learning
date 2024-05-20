e6dcb8cf751ad9b24ea478a02e2da8a644f0e033
Harshit Rai <workwithharshitrai@gmail.com>
2024-05-20 23:35:48 +0530

    Iterations continued

--- iterations.js
+++ iterations.js
@@ -72,3 +72,72 @@ do {
     console.log(`Score is ${score}`);
     score++
 } while (score <= 10);
+
+
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr1 = [1, 2, 3, 4, 5]
+
+for (const num of arr1) {
+    //console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    //console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
+
+const myObj = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+const mp = new Map()
+mp.set('IN', "India")
+mp.set('USA', "United States of America")
+mp.set('Fr', "France")
+mp.set('IN', "India")
+
+for (const key in mp) {
+    console.log(key);
+}
