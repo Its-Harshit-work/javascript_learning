b6b0bd64e0fa6e139b590f2b2b6406fc5e16a91a
Harshit Rai <workwithharshitrai@gmail.com>
2024-05-31 19:12:48 +0530

    changes in file.js

---
+++ file1.js
@@ -0,0 +1,76 @@
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
+
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
